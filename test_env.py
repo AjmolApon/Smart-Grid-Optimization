b4fd315bf9717166ebdc907764ae3677ba9e35e9
Ajmol Apon <ajmolapon@Ajmols-MacBook-Air.local>
2026-09-04 11:37:53 +0600

    Initial commit: Project setup, dependencies, and test script

---
+++ test_env.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import pulp
+import streamlit as st
+import xgboost as xgb
+
+print("✅ pandas version:", pd.__version__)
+print("✅ numpy version:", np.__version__)
+print("✅ xgboost version:", xgb.__version__)
+print("✅ PuLP (Optimization) version:", pulp.__version__)
+print("✅ streamlit version:", st.__version__)
+print("\n🎉 চমৎকার! আপনার স্মার্ট গ্রিড প্রজেক্টের এনভায়রনমেন্ট সম্পূর্ণ প্রস্তুত।")
